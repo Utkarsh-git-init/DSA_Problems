51251a7f6b34f6966a5b45a094a1ab401162e486
Utkarsh-git-init <utmaurya2312@gmail.com>
2026-03-23 23:27:31 +0530

    added leetcode problem 114 Flatten Binary Tree to linked list

--- BinaryTrees/FlattenBinaryTreeToLinkedList114/Solution.java
+++ BinaryTrees/FlattenBinaryTreeToLinkedList114/Solution.java
@@ -1,7 +1,35 @@
 package Strivers_dsa.BinaryTrees.FlattenBinaryTreeToLinkedList114;
 
-public class Solution {
-    public void flatten(TreeNode root) {
 
+public class Solution {
+    void swap(TreeNode node){
+        if(node==null)
+            return;
+        TreeNode temp=node.left;
+        node.left=node.right;
+        node.right=temp;
+        swap(node.left);
+        swap(node.right);
+    }
+    TreeNode convert(TreeNode node){
+        if(node==null||(node.left==null&&node.right==null))
+            return node;
+        TreeNode right=convert(node.right);
+        TreeNode left=convert(node.left);
+        if(left==null)
+            return right;
+        else if(right==null) {
+            node.right = node.left;
+            node.left=null;
+        }
+        else {
+            right.right =node.left;
+            node.left=null;
+        }
+        return left;
+    }
+    public void flatten(TreeNode root) {
+        swap(root);
+        convert(root);
     }
 }
