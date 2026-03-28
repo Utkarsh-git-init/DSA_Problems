e0382c91f974f5f1e86ee335064eb66405c63554
Utkarsh-git-init <utmaurya2312@gmail.com>
2026-03-28 22:54:10 +0530

    added leetcode problem 173 BST Iterator

---
+++ BinarySearchTrees/BinarySearchTreeIterator173/BSTIterator.java
@@ -0,0 +1,42 @@
+package Strivers_dsa.BinarySearchTrees.BinarySearchTreeIterator173;
+
+import java.util.Stack;
+
+public class BSTIterator {
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack=new Stack<>();
+        stack.push(root);
+        while(stack.peek()!=null){
+            TreeNode node=stack.pop();
+            stack.push(node.right);
+            stack.push(node);
+            stack.push(node.left);
+        }
+    }
+
+    public int next() {
+        while (!stack.isEmpty()&&stack.peek()!=null){
+            TreeNode temp=stack.pop();
+            stack.push(temp.right);
+            stack.push(temp);
+            stack.push(temp.left);
+        }
+        stack.pop();
+        return stack.pop().val;
+    }
+
+    public boolean hasNext() {
+        if(!stack.isEmpty()){
+            if(stack.peek()==null){
+                stack.pop();
+                if(stack.isEmpty())
+                    return false;
+                stack.push(null);
+                return true;
+            }
+            return true;
+        }
+        return false;
+    }
+}
